96c97804713a513d8a98dccfb649645e5cfaca4d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-30 18:26:57 -0700

    add placeholder component

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ReusableForm(){
+  return(
+    <React.Fragment>
+      <form>
+        <h1> form will go here... inputs and props</h1>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default ReusableForm;
