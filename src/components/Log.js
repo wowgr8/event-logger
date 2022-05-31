31b0493c98fcbda18e2cad3819f46d47c13559a6
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-30 18:30:07 -0700

    add placeholder for Log component, also added notes

---
+++ src/components/Log.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import proptypes
+
+function Log(){
+  return(
+    <React.Fragment>
+      <h1> This should show info that was submitted via NewLog.js using ReusableForm. </h1>
+      <h1> Props will be needed</h1>
+    </React.Fragment>
+  )
+}
+
+export default Log;
