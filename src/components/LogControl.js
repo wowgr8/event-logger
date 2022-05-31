7cacf4a59a0fde33493afcb1c7e0ad4ccf1df030
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-30 18:12:19 -0700

    add class component for LogControl with placeholders - state will live here

---
+++ src/components/LogControl.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import NewLog
+// import LogLIst
+// import LogDetails
+// IMport EditLog
+
+class LogControl extends React.Component{
+
+  // constructor w/ props and state goes here
+
+  //Methods for buttons go here
+
+
+
+  render(){
+    //conditionals go here
+
+
+
+    return(
+      <React.Fragment>
+        <h2>This is the currently visible view/log variable placeholder</h2>
+        <h3> post button placeholder</h3>
+      </React.Fragment>
+    );
+  }
+}
+
+export default LogControl;
