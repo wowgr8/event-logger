d4dcabd89d1581b90edf4b54079286e6217e5283
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-30 18:45:47 -0700

    add placeholder for NewLog component, also added notes

---
+++ src/components/NewLog.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import UUID
+// import proptypes
+// import reusableForm
+
+function NewLog(){
+
+  // function for eventhandler will go here (it's being passed down from control)
+
+
+  return(
+    <React.Fragment>
+      <ReusableForm
+        // <p>button to submirt new log will go here</p>
+        />
+    </React.Fragment>
+  );
+}
+
+export default NewLog;
