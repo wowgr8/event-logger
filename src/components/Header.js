cb8a13de82343d34ffe0e0f8bc26b94451abe5eb
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-30 18:00:17 -0700

    add place-holder for header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+  return(
+    <React.Fragment>
+      <h1>BirdWatch Logger (PlaceHolder for Header)</h1>
+    </React.Fragment>
+  )
+}
+
+export default Header;
