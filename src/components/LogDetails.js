9ad3d6c01a97c701292238612978401175abe8d7
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-30 18:36:47 -0700

    add placeholder for LogDetails component, also added notes

---
+++ src/components/LogDetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import props
+
+function LogDetails(){
+
+  return(
+    <React.Fragment>
+      <h1>Log Details</h1>
+      {/* key values/ code placeholder when props are implemented */}
+      <button></button>
+    </React.Fragment>
+  )
+}
+
+export default LogDetails;
