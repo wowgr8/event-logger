369be1ad2f56a40248c876b7b875f54cbca2ee92
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-05-30 18:33:29 -0700

    add placeholder for EditLog component, also added notes

---
+++ src/components/EditLog.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import reusable form
+// import proptypes
+
+function EditLog(){
+  return(
+    <React.Fragment>
+      {/* some kind of event handler */}
+      <p>an edit button</p>
+    </React.Fragment>
+  )
+}
+
+export default EditLog;
