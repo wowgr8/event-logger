ab38caa673fd8c30119ba2e429dd4732086c5c79
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-01 22:19:40 -0700

    add new variable for button and set to null. Added button to else part of conditional so if formvisibleonpage is set to false, we'll set the value of addticketbutton variable to the button with it's click handler. Also added addticketbutton to return so it reflects in DOM

--- src/components/LogControl.js
+++ src/components/LogControl.js
@@ -20,20 +20,172 @@ class LogControl extends React.Component{
 
   render(){
     let currentlyVisibleState = null;
+    let addLogButton = null;
     if(this.state.formVisibleOnPage){
       currentlyVisibleState = <NewLog />
     } else {
       currentlyVisibleState = <LogList />
+      addLogButton = <button onClick={this.handleClick}>Add Log</button>
     }
 
 
     return(
       <React.Fragment>
         {currentlyVisibleState}
-        <h3> post button placeholder</h3>
+        {addLogButton}
       </React.Fragment>
     );
   }
 }
 
 export default LogControl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// WHAT IT COULD LOOK LIKE --- JOHN W. Example
+
+// import React from 'react';
+// import NewMerchForm from './NewMerchForm';
+// import MerchList from './MerchList';
+// import MerchDetail from './MerchDetail';
+// import EditMerchForm from './EditMerchForm';
+
+// class MerchControl extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       formVisibleOnPage: false,
+//       mainMerchList: [],
+//       selectedMerch: null,
+//       editing: false
+//     };
+//   }
+
+//   handleClick = () => {
+//     if (this.state.selectedMerch != null) {
+//       this.setState({
+//         formVisibleOnPage: false,
+//         selectedMerch: null,
+//         editing: false
+//       });
+//     } else {
+//       this.setState(prevState => ({
+//         formVisibleOnPage: !prevState.formVisibleOnPage 
+//       }));
+//     } 
+//   }
+
+//   handleAddingNewMerchToList = (newMerch) => {
+//     const newMainMerchList = this.state.mainMerchList.concat(newMerch);
+//     this.setState({
+//       mainMerchList: newMainMerchList,
+//       formVisibleOnPage: false
+//     });
+//   }
+
+//   handleChangingSelectedMerch = (id) => {
+//     const selectedMerch =this.state.mainMerchList.filter(merch => merch.id === id)[0];
+//     this.setState({selectedMerch: selectedMerch});
+//   }
+
+//   handleDeletingMerch = (id) => {
+//     const newMainMerchList = this.state.mainMerchList.filter(merch => merch.id !== id);
+//     this.setState({
+//       mainMerchList: newMainMerchList,
+//       selectedMerch: null
+//     });
+//   }
+
+//   handleEditClick = () => {
+//     this.setState({editing:true});
+//   }
+
+//   handleEditingMerchInList = (merchToEdit) => {
+//     const editedMainMerchList = this.state.mainMerchList
+//       .filter(merch => merch.id !== this.state.selectedMerch.id)
+//       .concat(merchToEdit);
+//     this.setState({
+//       mainMerchList: editedMainMerchList,
+//       editing: false,
+//       selectedMerch: null
+//     });
+//   }
+
+//   handleIncreasingMerchStock = (id) => {
+//     if(this.state.mainMerchList > 1) {
+//       const selectedMerch = this.state.mainMerchList.filter(merch => merch.id === id)[0]
+//       selectedMerch.items++;
+//       const newMainMerchList = this.state.mainMerchList.filter(merch => merch.id !==id).concat(selectedMerch);
+//       this.setState({mainMerchList:newMainMerchList});
+//     } else {
+//       const selectedMerch = this.state.mainMerchList.filter(merch => merch.id === id)[0]
+//       selectedMerch.items++;
+//       const newMerchListArray = [];
+//       const changedMerchArray = newMerchListArray.concat(selectedMerch);
+//       this.setState({
+//         mainMerchList: changedMerchArray
+//       });
+//     }
+//   }
+
+//   handleDecreasingMerchStock = (id) => {
+//     if(this.state.mainMerchList > 1) {
+//       const selectedMerch = this.state.mainMerchList.filter(merch => merch.id === id)[0]
+//       selectedMerch.items--;
+//       const newMainMerchList = this.state.mainMerchList.filter(merch => merch.id !==id).concat(selectedMerch);
+//       this.setState({mainMerchList:newMainMerchList});
+//     } else {
+//       const selectedMerch = this.state.mainMerchList.filter(merch => merch.id === id)[0]
+//       selectedMerch.items--;
+//       const newMerchListArray = [];
+//       const changedMerchArray = newMerchListArray.concat(selectedMerch);
+//       this.setState({
+//         mainMerchList: changedMerchArray
+//       });
+//     }
+//   }
+
+//   render() {
+//     let currentlyVisibleState = null;
+//     let buttonText = null;
+//     if (this.state.editing) {
+//       currentlyVisibleState = <EditMerchForm merch = {this.state.selectedMerch} onEditMerch = {this.handleEditingMerchInList}/>
+//       buttonText = "Return to Merch List";
+//     } else if(this.state.selectedMerch != null) {
+//       currentlyVisibleState = <MerchDetail 
+//         merch={this.state.selectedMerch}
+//         onClickingDecrease={this.handleDecreasingMerchStock}
+//         onClickingIncrease={this.handleIncreasingMerchStock}
+//         onClickingDelete={this.handleDeletingMerch}
+//         onClickingEdit={this.handleEditClick} />
+//       buttonText= "Return to Merch List";
+//     } else if (this.state.formVisibleOnPage) {
+//       currentlyVisibleState = <NewMerchForm onNewMerchCreation={this.handleAddingNewMerchToList}/>
+//       buttonText = "Return to Merch List";
+//     } else {
+//       currentlyVisibleState = <MerchList merchList={this.state.mainMerchList} onMerchSelection = {this.handleChangingSelectedMerch}/>
+//       buttonText = "Add Merch";
+//     }
+
+//     return(
+//       <React.Fragment>
+//         {currentlyVisibleState}
+//         <button onClick={this.handleClick}>{buttonText}</button>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+// export default MerchControl;
