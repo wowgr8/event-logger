b1689046a85b88721b51d72406d4fbe09e64b120
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-06-01 23:12:15 -0700

    add form and event handler upon submitting with event.preventDefault. Also added a function for handling new log submission/click of button - currently only console.logs - WIP

--- src/components/NewLog.js
+++ src/components/NewLog.js
@@ -1,18 +1,34 @@
 import React from "react";
-// import UUID
+import { v4 } from 'uuid';
 // import proptypes
-import ReusableForm from "./ReusableForm";
+// import ReusableForm from "./ReusableForm";
 
 function NewLog(){
 
   // function for eventhandler will go here (it's being passed down from control)
-
+  function handleNewLogSubmission(event){
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.log.value);
+  }
 
   return(
     <React.Fragment>
-      <ReusableForm
-        // <p>button to submirt new log will go here</p>
-        />
+      <form onSubmit={handleNewLogSubmission}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Bird Name' />
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <textarea
+          name='log'
+          placeholder='Log  bird details.' />
+        <button type='submit'>Log!</button>
+      </form>
     </React.Fragment>
   );
 }
